ed4e8dfd79318443b4dccb5e8d3fa2023e5d8dc3
davood bereihi <d.bereihi@gmail.com>
2022-01-15 23:25:55 +0330

    Update Dx.php

--- Dx.php
+++ Dx.php
@@ -15,8 +15,6 @@ JLoader::register('FieldsHelper', JPATH_ADMINISTRATOR . '/components/com_fields/
 
 class Dx
 {
-
-	
 	private static $_instance;
 	private $document;
 	private $importedFiles = array();
